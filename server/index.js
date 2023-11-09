773ea37ce548da89992d5e0698bc793bfc83877e
fadi <fadi.m.marouf@gmail.com>
2023-11-09 17:52:26 +0100

    import register

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-
+import {register} from "./controllers/auth.js"
 // CONFIGURATIONS
 
 const __filename = fileURLToPath(import.meta.url);
@@ -36,8 +36,11 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-// MONGOOSE SETUP
+//ROUTES WITH FILES
+app.post("/auth/register", upload.single('picture'),register)
 
+
+// MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
 mongoose
     .connect(process.env.MONGO_URL)
