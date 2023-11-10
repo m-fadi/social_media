4ea45c37b3da18ba846082d1d2246e4666b0ab77
fadi <fadi.m.marouf@gmail.com>
2023-11-10 04:28:24 +0100

    import routes

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
+import authRoutes from './routes/auth.js'
 // CONFIGURATIONS
 
 const __filename = fileURLToPath(import.meta.url);
