ac1cd248590c5ef9c54f548bc5c589553530f58c
fadi <fadi.m.marouf@gmail.com>
2023-11-10 04:23:09 +0100

    clea

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import {register} from "./controllers/auth.js"
+import { register } from "./controllers/auth.js";
 // CONFIGURATIONS
 
 const __filename = fileURLToPath(import.meta.url);
@@ -37,8 +37,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 //ROUTES WITH FILES
-app.post("/auth/register", upload.single('picture'),register)
-
+app.post("/auth/register", upload.single("picture"), register);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
